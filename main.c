d4299ce4533a7640dde7a25157bee68daa6e0f73
Samy HALIT <e12200614@f208-2.ig-edu.univ-paris13.fr>
2025-02-04 10:40:43 +0100

    1er parseur

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-extern int yyparse(voix);
+extern int yyparse(void);
 int main(void){
     if (!yyparse()) {
         printf("\nParsing:: c'est bien une expression artihmétique\n);");
