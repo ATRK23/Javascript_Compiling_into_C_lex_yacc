8f1c5229a0838e8f9c466e6d6bfd21972902d993
Samy HALIT <e12200614@f208-2.ig-edu.univ-paris13.fr>
2025-03-18 11:48:51 +0100

    Exo 5, Question 2

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(void){
     AST_expr temp;
     AST_comm arbre = new_command(temp);
     if (!yyparse(&arbre)) {
+        printf("\n");
         print_code(arbre);
     }
     exit(EXIT_SUCCESS);
