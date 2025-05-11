e7eb78f08856f142cc55de33cc1f7b1ce2fa618b
Samy HALIT <samy.halit@edu.univ-paris13.fr>
2025-05-11 23:44:16 +0200

    Exo 6, fin du TP1 :)

--- main.c
+++ main.c
@@ -2,12 +2,27 @@
 #include <stdio.h>
 #include "AST.h"
 extern int yyparse(void*);
-int main(void){
+extern FILE* yyin;
+
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <fichier en entrée>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+        perror("Erreur ouverture fichier");
+        return EXIT_FAILURE;
+    }
+
     AST_expr temp;
     AST_comm arbre = new_command(temp);
     if (!yyparse(&arbre)) {
         printf("\n");
         print_code(arbre);
     }
+    
+    fclose(yyin);
     exit(EXIT_SUCCESS);
-} 
+}
